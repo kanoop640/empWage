e8e3630e63368a53afa8a677b5da55e8d2fcbf09
kanoop640 <kanoop640@gmail.com>
2020-02-12 11:20:00 +0530

    [Anoop]Added.EmpDailyWage

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -17,28 +17,3 @@ wage=$(echo "$hours" "20" |awk '{print $1*$2}')
  	echo "Daily wage is : " $wage
 	return $wage
 }
-partTimeEmpWage()
-{
-	read -p "Enter number of hours to be done extra " part
-	wage=$(( part*20 ))
-	echo $wage
-	return  $wage
-}
-
-empWageForMonth() 
-{
-	read -p "Enter the number of months in which work is done " month
-	TotalWage=$(echo "$month" "20" |awk '{print $1*$2*8*$2}')
-	echo "Total wage in " $month " month is : " $TotalWage
-}
-empWageMonthCondition()
-{
-	read -p "Enter number of hours done in  month " hours
-	if(( $hours<101 ))
-	then
-		wage=$(echo )
-}
-Attendence
-empDailyWage 
-partTimeEmpWage
-empWageForMonth
